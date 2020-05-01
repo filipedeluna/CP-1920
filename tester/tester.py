6da8818a0f2ce3f87f9f4046a2998cf015c602a4
Filipe de Luna <f.luna@campus.fct.unl.pt>
2020-05-01 22:34:14 +0100

    started work on plot

--- tester/tester.py
+++ tester/tester.py
@@ -3,11 +3,12 @@ import sys
 import array
 
 import numpy
-from numpy import mean
+import matplotlib
+import matplotlib.pyplot as pyplot
 
 # Constants
 TESTS = 14
-ITERATIONS = [10, 50, 100, 500, 1000, 5000, 10000]
+ITERATIONS = [10, 50, 100, 500, 1000, 5000, 10000, 50000]
 THREADS = [1, 2, 4, 8, 16, 32, 64, 128]
 REPETITIONS = 5
 
@@ -27,7 +28,6 @@ if len(sys.argv) > 2:
     print("Too many arguments - expected test number")
     sys.exit(-1)
 
-
 if not sys.argv[1].isdecimal():
     print("Expected test number - must be integer")
     sys.exit(-1)
@@ -40,27 +40,38 @@ if testID < 1 or testID > TESTS + 1:
 
 results = []
 totalTests = len(ITERATIONS) * len(THREADS)
+testName = ""
 
-for i in range(0, len(ITERATIONS)):
-    iterationResults = []
-    for t in range(0, len(THREADS)):
-        repetitionResults = []
+for t in range(0, len(THREADS)):
+    threadResults = []
+    for i in range(0, len(ITERATIONS)):
+        itResults = []
         for r in range(0, REPETITIONS):
             stream = os.popen(f"{PROGRAM} -i {ITERATIONS[i]} -k {testID} -t {THREADS[t]}")
             output = stream.read().split("Done!\n\n")
             time = output[1].split(":\t")[1].split(" micro")[0]
 
-            repetitionResults.append(int(time))
+            itResults.append(int(time))
             testName = output[1].split(":\t")[0]
 
-            iterationResults.append(mean(repetitionResults))
+            currTest = i * (len(ITERATIONS) + 1) + t + 1
+            print(f"Finished test {currTest}/{totalTests}")
 
-        currTest = i * (len(ITERATIONS) + 1) + t + 1
-        print(f"Finished test {currTest}/{totalTests}")
-
-    results.append(iterationResults)
+        threadResults.append(numpy.mean(itResults))
+    results.append(threadResults)
 
 print(results)
 
+# Data for plotting
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
 
+for t in range(0, len(THREADS)):
+    ax.plot(results[t], label=f"{THREADS[t]} Threads")
 
+ax.set(xlabel='Time (microseconds)', ylabel='N. of Threads',
+       title=f"{testName}")
+ax.grid()
+
+pyplot.legend(loc=1)
+fig.savefig(f"{testName}.png")
